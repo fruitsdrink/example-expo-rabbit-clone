e630f64c50f78fbc740a589fa4a4b0b3b5426dab
fruitsdrink <fruitsdrink@126.com>
2025-09-21 21:58:46 +0800

    finish detail post page

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -1,5 +1,7 @@
-import { Redirect, Stack } from "expo-router";
+import { Redirect, router, Stack } from "expo-router";
 import { useAuth } from "@clerk/clerk-expo";
+import {AntDesign, Entypo, Ionicons, MaterialIcons} from '@expo/vector-icons'
+import { View } from "react-native";
 
 export default function ProtectedLayout() {
   const { isSignedIn } = useAuth();
@@ -10,5 +12,20 @@ export default function ProtectedLayout() {
 
   return <Stack>
     <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    <Stack.Screen name="post/[id]" options={{ 
+      headerStyle: {
+        backgroundColor: '#ff5700'
+      },
+      animation: 'slide_from_bottom',
+      headerBackButtonDisplayMode: 'minimal',
+      headerLeft: ()=> <AntDesign name="close" size={24} color="white" onPress={()=> router.back()} />,
+      headerRight: ()=>(
+        <View style={{flexDirection: 'row', gap: 10}}>
+          <Ionicons name="search" size={24} color="white" />
+          <MaterialIcons name="sort" size={24} color="white" />
+          <Entypo name="dots-three-vertical" size={24} color="white" />
+        </View>
+      )
+    }} />
   </Stack>
 }
--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
 import { formatDistanceToNowStrict } from "date-fns";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import React from "react";
@@ -9,89 +9,120 @@ type Props = {
   isDetailedPost?: boolean;
 };
 export const PostListItem: React.FC<Props> = ({ post, isDetailedPost }) => {
+  const shouldShowImage = isDetailedPost || post.image;
+  const shouldShowDescription = isDetailedPost || !post.image;
+
   return (
     <Link href={`/post/${post.id}`}>
-    <View
-      style={{
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-      }}
-    >
-      {/* header */}
-      <View style={{ flexDirection: "row", gap: 10 }}>
-        <Image source={{ uri: post.group.image }} style={styles.image} />
-        <Text style={{ fontWeight: "bold" }}>{post.group.name}</Text>
-        <Text style={{ color: "gray" }}>
-          {formatDistanceToNowStrict(new Date(post.created_at))}
-        </Text>
-        <View style={{ marginLeft: "auto" }}>
-          <Text style={styles.joinButtonText}>Join</Text>
+      <View
+        style={{
+          paddingHorizontal: 15,
+          paddingVertical: 10,
+          gap: 7,
+          borderBottomColor: "lightgrey",
+          borderBottomWidth: 1,
+        }}
+      >
+        {/* header */}
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Image source={{ uri: post.group.image }} style={styles.image} />
+          <View>
+            <View style={{ flexDirection: "row", gap: 5 }}>
+              <Text
+                style={{ fontWeight: "bold", fontSize: 13, color: "#3a3b3c" }}
+              >
+                {post.group.name}
+              </Text>
+              <Text
+                style={{ color: "gray", fontSize: 13, alignSelf: "flex-start" }}
+              >
+                {formatDistanceToNowStrict(new Date(post.created_at))}
+              </Text>
+            </View>
+            {isDetailedPost && (
+              <Text style={{ fontSize: 13, color: "#2e5daa" }}>
+                {post.user.name}
+              </Text>
+            )}
+          </View>
+          <Pressable
+            style={{
+              marginLeft: "auto",
+              backgroundColor: "#0d469b",
+              borderRadius: 20,
+            }}
+          >
+            <Text style={styles.joinButtonText}>Join</Text>
+          </Pressable>
         </View>
-      </View>
-      {/* content */}
-      <Text style={styles.title}>{post.title}</Text>
-      {post.image && (
-        <Image
-          source={{ uri: post.image }}
-          style={{
-            width: "100%",
-            aspectRatio: 4 / 3,
-            borderRadius: 15,
-            objectFit: "cover",
-          }}
-        />
-      )}
-      {!post.image && post.description && <Text>{post.description}</Text>}
-      {/* footer */}
-      <View style={{ flexDirection: "row" }}>
-        <View style={{ flexDirection: "row", gap: 10 }}>
-          <View style={[{ flexDirection: "row" }, styles.iconBox]}>
+        {/* content */}
+        <Text style={styles.title}>{post.title}</Text>
+        {shouldShowImage && post.image && (
+          <Image
+            source={{ uri: post.image }}
+            style={{
+              width: "100%",
+              aspectRatio: 4 / 3,
+              borderRadius: 15,
+              objectFit: "cover",
+            }}
+          />
+        )}
+        {shouldShowDescription && post.description && (
+          <Text numberOfLines={isDetailedPost ? undefined : 4}>
+            {post.description}
+          </Text>
+        )}
+        {/* footer */}
+        <View style={{ flexDirection: "row" }}>
+          <View style={{ flexDirection: "row", gap: 10 }}>
+            <View style={[{ flexDirection: "row" }, styles.iconBox]}>
+              <MaterialCommunityIcons
+                name="arrow-up-bold-outline"
+                size={19}
+                color={"black"}
+              />
+              <Text>{post.upvotes}</Text>
+              <View
+                style={{
+                  width: 1,
+                  backgroundColor: "#d4d4d4",
+                  height: 14,
+                  marginHorizontal: 7,
+                  alignSelf: "center",
+                }}
+              ></View>
+              <MaterialCommunityIcons
+                name="arrow-down-bold-outline"
+                size={19}
+                color={"black"}
+              />
+            </View>
+            <View style={[{ flexDirection: "row" }, styles.iconBox]}>
+              <MaterialCommunityIcons
+                name="comment-outline"
+                size={19}
+                color={"black"}
+              />
+              <Text>{post.nr_of_comments}</Text>
+            </View>
+          </View>
+          <View style={{ flexDirection: "row", marginLeft: "auto", gap: 10 }}>
             <MaterialCommunityIcons
-              name="arrow-up-bold-outline"
+              name="trophy-outline"
               size={19}
               color={"black"}
+              style={styles.iconBox}
             />
-            <Text>{post.upvotes}</Text>
-            <View
-              style={{
-                width: 1,
-                backgroundColor: "#d4d4d4",
-                height: 14,
-                marginHorizontal: 7,
-                alignSelf: "center",
-              }}
-            ></View>
             <MaterialCommunityIcons
-              name="arrow-down-bold-outline"
+              name="share-outline"
               size={19}
               color={"black"}
+              style={styles.iconBox}
             />
           </View>
-          <View style={[{ flexDirection: "row" }, styles.iconBox]}>
-            <MaterialCommunityIcons
-              name="comment-outline"
-              size={19}
-              color={"black"}
-            />
-            <Text>{post.nr_of_comments}</Text>
-          </View>
-        </View>
-        <View style={{ flexDirection: "row", marginLeft: "auto", gap: 10 }}>
-          <MaterialCommunityIcons
-            name="trophy-outline"
-            size={19}
-            color={"black"}
-            style={styles.iconBox}
-          />
-          <MaterialCommunityIcons
-            name="share-outline"
-            size={19}
-            color={"black"}
-            style={styles.iconBox}
-          />
         </View>
       </View>
-    </View>
     </Link>
   );
 };
@@ -104,11 +135,13 @@ const styles = StyleSheet.create({
     paddingHorizontal: 7,
     borderRadius: 10,
     fontWeight: "bold",
+    fontSize: 13,
   },
   image: {
     width: 20,
     height: 20,
     borderRadius: 20,
+    marginRight: 5,
   },
   title: {
     fontWeight: "bold",
